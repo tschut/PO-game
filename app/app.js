8ab613a54bcf51ee944bc29d043e0ee8c994b69b
tschut <tschut@gmail.com>
2016-03-10 14:52:22 +0100

    Move backlog to a service

--- app/app.js
+++ app/app.js
@@ -5,6 +5,20 @@ var poGame = angular.module('poGame', [
   'rzModule'
   ]);
 
+var BACKLOG = [
+  {"id": 1, "description": "1-click checkout", "value": 1, "estimate": 3},
+  {"id": 2, "description": "suggestions based on previous purchases", "value": 3, "estimate": 3},
+  {"id": 3, "description": "password reset", "value": 5, "estimate": 8}
+];
+
+angular.module('poGame').factory('game', function() {
+  var game = {};
+
+  game.backlog = BACKLOG;
+
+  return game;
+});
+
 poGame.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
--- app/controllers.js
+++ app/controllers.js
@@ -1,13 +1,7 @@
 var poGameControllers = angular.module('poGameControllers', []);
 
-var BACKLOG = [
-	{"id": 1, "description": "1-click checkout", "value": 1, "estimate": 3},
-	{"id": 2, "description": "suggestions based on previous purchases", "value": 3, "estimate": 3},
-	{"id": 3, "description": "password reset", "value": 5, "estimate": 8}
-];
-
-poGameControllers.controller('BacklogCtrl', function ($scope) {
-	$scope.backlog = BACKLOG;
+poGameControllers.controller('BacklogCtrl', function ($scope, game) {
+	$scope.backlog = game.backlog;
 });
 
 poGameControllers.controller('SprintParamCtrl', function ($scope) {
