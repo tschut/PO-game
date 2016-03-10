2d953a0c2654ca6add448045de46b545fa3a1c16
tschut <tschut@gmail.com>
2016-03-10 19:24:00 +0100

    Correctly handle backlog in sprint cycle

--- app/app.js
+++ app/app.js
@@ -28,14 +28,16 @@ angular.module('poGame').factory('game', function() {
     completedStories = [];
 
     spcount = 0;
+    completedIndexes = [];
     for (i = 0; i < game.backlog.length; ++i) {
       spcount += game.backlog[i].estimate;
       if (spcount > storypointsCompleted) {
         break;
       };
 
-      completedStories.push(game.backlog[i]);
-      game.backlog[i].completed = true;
+      newStory = jQuery.extend(true, {}, game.backlog[i]);
+      console.log(newStory);
+      completedStories.push(newStory);
     }
 
     storypointsCompleted = 0;
@@ -43,10 +45,16 @@ angular.module('poGame').factory('game', function() {
     completedStories.forEach(function (story) {
       storypointsCompleted += story.estimate;
       valueDelivered += story.value;
+
+      console.log('filter', game.backlog.filter(function(backlog_story) {return backlog_story.id != story.id}));
+      game.backlog = game.backlog.filter(function(backlog_story) {return backlog_story.id != story.id});
     });
     game.sprintresults.storypoints = storypointsCompleted;
     game.sprintresults.valueDelivered = valueDelivered;
     game.sprintresults.completedStories = completedStories;
+
+    console.log('backlog', game.backlog)
+    console.log('completed', game.sprintresults.completedStories)
   }
 
   return game;
@@ -66,6 +74,10 @@ poGame.config(['$routeProvider',
     when('/sprint-review', {
       templateUrl: 'partials/sprint-review.html',
       controller: 'SprintReviewCtrl'
+    }).
+    when('/sprint-planning', {
+      templateUrl: 'partials/sprint-planning.html',
+      controller: 'BacklogCtrl'
     }).
     otherwise({
       redirectTo: '/initial-product-backlog'
