ddb3e47c5fc799b93ceb4760b542f172499bee07
tschut <tschut@gmail.com>
2016-03-10 09:24:51 +0100

    Add value and estimate to backlogitems

--- app/controllers.js
+++ app/controllers.js
@@ -1,9 +1,9 @@
 var poGame = angular.module('poGame', ['ng-sortable']);
 
 var BACKLOG = [
-	{"id": 1, "description": "1-click checkout"},
-	{"id": 2, "description": "suggestions based on previous purchases"},
-	{"id": 3, "description": "password reset"}
+	{"id": 1, "description": "1-click checkout", "value": 1, "estimate": 3},
+	{"id": 2, "description": "suggestions based on previous purchases", "value": 3, "estimate": 3},
+	{"id": 3, "description": "password reset", "value": 5, "estimate": 8}
 ];
 
 poGame.controller('BacklogCtrl', function ($scope) {
