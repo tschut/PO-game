fdcc32ccf517ed13432e517fbbedbc683e39d570
tschut <tschut@gmail.com>
2016-03-21 16:27:08 +0100

    Refactor: backlog to Game class

--- app/app.js
+++ app/app.js
@@ -7,22 +7,12 @@ var poGame = angular.module('poGame', [
   'rzModule'
   ]);
 
-var BACKLOG = [
-  {"id": 1, "description": "1-click checkout", "value": 1, "estimate": 3},
-  {"id": 2, "description": "Suggestions based on previous purchases", "value": 3, "estimate": 3},
-  {"id": 3, "description": "Password reset", "value": 5, "estimate": 8},
-  {"id": 4, "description": "View basket", "value": 5, "estimate": 8},
-  {"id": 5, "description": "Remove items from basket", "value": 5, "estimate": 8},
-  {"id": 6, "description": "Title-based search", "value": 5, "estimate": 8},
-];
-
 angular.module('poGame').factory('game', function() {
   var game = new Game();
   game.state = {};
 
   game.state.weeksleft = 26;
 
-  game.backlog = BACKLOG;
   game.sprintresults = {};
 
   game.sprintparams = {'duration': '2'}
--- app/game.js
+++ app/game.js
@@ -1,3 +1,12 @@
+var BACKLOG = [
+  {"id": 1, "description": "1-click checkout", "value": 1, "estimate": 3},
+  {"id": 2, "description": "Suggestions based on previous purchases", "value": 3, "estimate": 3},
+  {"id": 3, "description": "Password reset", "value": 5, "estimate": 8},
+  {"id": 4, "description": "View basket", "value": 5, "estimate": 8},
+  {"id": 5, "description": "Remove items from basket", "value": 5, "estimate": 8},
+  {"id": 6, "description": "Title-based search", "value": 5, "estimate": 8},
+];
+
 function Game() {
-	
+	this.backlog = BACKLOG;
 }
